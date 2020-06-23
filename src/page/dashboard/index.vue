3008bf36488097ebaecd0bc99b3a5c87f8d83122
Aries <296992400@qq.com>
2020-06-23 18:05:46 +0800

    修改部分界面、

--- src/page/dashboard/index.vue
+++ src/page/dashboard/index.vue
@@ -12,36 +12,34 @@
         <Box2 title="特殊危险源监控" autoSize="true" titleSize="1" class="box" style="top:73%;left:50px;">
           <echarts :option="tswxy"></echarts>
         </Box2>
-        <Box2
+        <!-- <Box2
           title="运输车实时监控"
           autoSize="true"
           titleSize="2"
           class="box-bottom"
           style="top:60%;right:50px;left:32%;"
         >
-          <!-- <dv-scroll-board :config="cljk" style="height:80%;color:white;" />   -->
           <echarts :option="ssyj"></echarts>
-        </Box2>
+        </Box2> -->
         <Box2 title="实时警报统计" autoSize="true" class="boxD" style="top:13%;right:50px;">
           <echarts :option="bjtj" style="margin-top:20px"></echarts>
         </Box2>
         <Box2 title="监控平台使用状态" autoSize="true" class="box" style="top:73%;right:50px;">
           <dv-water-level-pond :config="jkxt" style="width:50%;height:80%" />
         </Box2>
-        <!-- 几个数字 -->
-        <div class="topNum" style="top:16%;left:30%;backgroud-color:red;">
+        <div class="topNum" style="top:13%;left:30%;border:2px solid #1adedd !important;">
           <div class="topNumTitle">253</div>
           <div class="topNumSubTitle">成功避免了253次事故</div>
         </div>
-        <div class="topNum" style="top:16%;left:40%">
+        <div class="topNum" style="top:13%;left:40%">
           <div class="topNumTitle">965</div>
           <div class="topNumSubTitle">保障965次危险品运输</div>
         </div>
-        <div class="topNum" style="top:16%;left:50%">
+        <div class="topNum" style="top:13%;left:50%">
           <div class="topNumTitle">85</div>
           <div class="topNumSubTitle">覆盖85家危运公司</div>
         </div>
-        <div class="topNum" style="top:16%;left:60%">
+        <div class="topNum" style="top:13%;left:60%">
           <div class="topNumTitle">302</div>
           <div class="topNumSubTitle">实时追踪302条运输线路</div>
         </div>
@@ -94,16 +92,36 @@ export default {
     changeSpin() {
       this.isSpin = !this.isSpin;
     },
-    onClick(event, instance, echarts) {
-      console.log(event);
-      console.log(instance);
-      console.log(echarts);
-    }
   }
 };
 </script>
 
 <style scoped>
+
+.topNum {
+  position: fixed;
+  width: 160px;
+  height: 140px;
+  color: white;
+  align-items: center;
+  border:2px solid #08636f;
+  border-radius: 4px;
+  padding:10px;
+  cursor: pointer;
+}
+.topNumTitle {
+  font-size: 30px;
+  font-weight: bold;
+  align-items: center;
+  text-align: center;
+  line-height: 70px;
+
+}
+.topNumSubTitle {
+  font-size: 15px;
+  align-items: center;
+  text-align: center;
+}
 .menuButton {
   width: 100%;
   height: 50px;
@@ -154,21 +172,5 @@ export default {
 .slide-fade-leave-to {
   opacity: 0;
 }
-.topNum {
-  position: fixed;
-  width: 10%;
-  height: 26%;
-  color: white;
-  align-items: center;
-}
-.topNumTitle {
-  font-size: 30px;
-  align-items: center;
-  text-align: center;
-}
-.topNumSubTitle {
-  font-size: 15px;
-  align-items: center;
-  text-align: center;
-}
+
 </style>
